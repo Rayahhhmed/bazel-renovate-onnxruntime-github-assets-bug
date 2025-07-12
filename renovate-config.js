f5ba506289b560b9009c1fe0564e11642e6c1aaa
Raiyan Ahmed <raiyan@canva.com>
2025-07-12 12:45:47 +1000

    adding assets bug

--- renovate-config.js
+++ renovate-config.js
@@ -1,6 +1,13 @@
 module.exports = {   
-    "requireConfig": "ignored",
-    "onboarding": false,
-    "enabledManagers": ["bazel", "bazel-module", "custom.regex"],
-    "repositories": ["bazel-renovate-onnxruntime-github-assets-bug"]
+  requireConfig: "ignored",
+  onboarding: false,
+  platform: "github",
+  persistRepoData: true,
+  baseBranches: ["main"],
+  addLabels: ["renovate"],
+  branchConcurrentLimit: 0,
+  prConcurrentLimit: 0,
+  prHourlyLimit: 0,
+  enabledManagers: ["bazel", "bazel-module", "custom.regex"],
+  repositories: ["Rayahhhmed/bazel-renovate-onnxruntime-github-assets-bug"],
 }
